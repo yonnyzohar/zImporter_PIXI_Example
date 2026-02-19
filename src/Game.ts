4ea88f71e23d7c86345303964b6c5293b9e9a86a
Yonny Zohar <yonnyzohar@gmail.com>
2026-02-19 17:00:19 +0000

    feat: integrate ZTimeline and update asset loading
    - Added ZTimeline import from zimporter-pixi.
    - Changed asset loading path from "fontExample" to "robot".
    - Implemented playback for ZTimeline instances in the scene stage.

--- src/Game.ts
+++ src/Game.ts
@@ -1,12 +1,12 @@
 import * as PIXI from 'pixi.js';
-import { ZButton, ZContainer, ZScene, ZState } from 'zimporter-pixi';
+import { ZButton, ZContainer, ZScene, ZState, ZTimeline } from 'zimporter-pixi';
 import { ZSceneStack } from 'zimporter-pixi';
 export class Game {
 
     stage: PIXI.Container;
     constructor(stage: PIXI.Container, forceRenderFnc: Function | null = null) {
         this.stage = stage;
-        const arr = ["fontExample"];
+        const arr = ["robot"];
         let loadPath = (window as any).loadPath || "./assets/" + arr[0] + "/";
         console.log("Game constructor " + loadPath);
         let scene: ZScene = new ZScene("testScene");
@@ -14,6 +14,14 @@ export class Game {
             ZSceneStack.push(scene);
             scene.loadStage(this.stage);
             let sceneStage: ZContainer = scene.sceneStage;
+            for (const child of sceneStage.children) {
+                if (child instanceof ZTimeline) {
+                    child.play();
+                }
+            }
+
+
+
             forceRenderFnc?.();
         });
     }
